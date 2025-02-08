82bb408dc6222d69cfdd4f281cec5a88b62facb8
yadamoon <yared241993@gmail.com>
2025-02-08 16:14:35 +0300

    updated apiURL value

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false, // Set to true for production builds
-    apiUrl: 'http://localhost:3000/api/v1', // API URL for local development
+    apiUrl: 'https://yummy-cake-house-api.onrender.com/api/v1', // API URL for local development
     imageUploadUrl: 'http://localhost:3000/upload', // Image upload endpoint
     categories: ['Cake', 'Pastry', 'Cookie', 'Cupcake', 'Beverage'], // Predefined categories for the product form
 };
